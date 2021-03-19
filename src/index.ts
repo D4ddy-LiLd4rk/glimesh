39cf1c2b7966c8ca2c7e29c8aff083b3e89b8540
D4ddy-LiLd4rk <D4ddyLiLd4rk@gmail.com>
2021-03-19 10:53:55 +0100

    Removed testing code

--- src/index.ts
+++ src/index.ts
@@ -13,35 +13,3 @@ export { User } from './API/User/User';
 export { AuthScopes } from './Tools'
 
 export * from './Auth/index';
-
-
-//=============================
-//Testing
-
-import { ApiClient } from './ApiClient';
-import { RefreshableAuthProvider, StaticAuthProvider, AuthProviderTokens } from './Auth/index';
-import { config } from './config';
-import { AuthScopes } from './Tools';
-
-let authProvider = new RefreshableAuthProvider(
-    new StaticAuthProvider(config.client_id,
-        config.access_token,
-        [AuthScopes.Public, AuthScopes.Email, AuthScopes.Chat, AuthScopes.Streamkey],
-        AuthProviderTokens.App), {
-    clientSecret: config.secret,
-    refreshToken: config.refresh_token,
-    onRefresh: ({ accessToken, refreshToken, expiryDate }) => {
-        console.log(`Access Token: ${accessToken}
-        Refresh Token: ${refreshToken}
-        Expiry Date: ${expiryDate}`);
-    }
-});
-let client = new ApiClient({ authProvider });
-
-client.channels.getChannelByName("jolan666")
-    .then(channel => {
-        console.log(channel);
-    })
-    .catch(err => {
-        console.log(err);
-    });
